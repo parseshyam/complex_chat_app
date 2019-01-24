7cb71e3311510143a30ae1c6ffb344abda305501
Parse Shyam <parse.shyam@SHYAM-PC.local>
2019-01-24 17:51:07 +0530

    updated server.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000 ;
 var app = express();
 
-app.use(port, () => {
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
     console.log('Server is up on port 3000 ');
 });
