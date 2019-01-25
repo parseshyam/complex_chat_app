0c77f4f98d1c01a203bc4a6dfff2a8f546a43731
Parse Shyam <parse.shyam@SHYAM-PC.local>
2019-01-25 12:42:04 +0530

    add form for messages and show incoming messages in browser

---
+++ public/js/index.js
@@ -0,0 +1,31 @@
+var socket = io();
+
+socket.on('connect',function(){
+    console.log('connected to server !');
+
+
+});
+
+socket.on('disconect',function(){
+    console.log('disconnected from server !');
+});
+
+
+socket.on('newMessage',function(message){
+    console.log('New message',message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from} : ${message.text}`);
+    jQuery('#messages').append(li);
+});
+
+
+jQuery('#message-form').on('submit',function(e){
+    e.preventDefault();
+
+    socket.emit('createMessage',{
+        from : 'user',
+        text : jQuery('[name=message]').val()
+    },function(){
+        // CALL BACK MESSAGE.
+    });
+});
--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 app.use(express.static(publicPath));
-
+ 
 
 io.on('connection',(socket) => {
     console.log('new user connected !');
