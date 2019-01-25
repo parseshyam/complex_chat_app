cc6c3b2c5ac4f0b5782ab3fe290ac5d25e399a7a
Parse Shyam <parse.shyam@SHYAM-PC.local>
2019-01-25 19:12:03 +0530

    add mustache for dynamic rendering

--- public/js/index.js
+++ public/js/index.js
@@ -15,20 +15,32 @@ socket.on('disconect',function(){
 
 socket.on('newMessage',function(message){
     var formattedTime = moment(message.createAt).format('h:mm a');
-    console.log('New message',message);
-    var li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-    jQuery('#messages').append(li);
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template,{
+        text : message.text,
+        from : message.from,
+        createdAt : formattedTime
+    });
+    jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage',function(message){
-    var li = jQuery('<li></li>');
-    var a =jQuery('<a target="_blank">My current location</a>');
+
     var formattedTime = moment(message.createAt).format('h:mm a');
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href',message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template,{
+        from : message.from,
+        url : message.url,
+        createdAt :formattedTime
+    });
+    jQuery('#messages').append(html);
+
+    // var li = jQuery('<li></li>');
+    // var a =jQuery('<a target="_blank">My current location</a>');
+    // li.text(`${message.from} ${formattedTime}: `);
+    // a.attr('href',message.url);
+    // li.append(a);
+    // jQuery('#messages').append(li);
 })
 
 jQuery('#message-form').on('submit',function(e){
