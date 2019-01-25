689e70aec4035ea24e2f389f7f8d11a43b7cac24
Parse Shyam <parse.shyam@SHYAM-PC.local>
2019-01-25 14:45:47 +0530

    add geolocation support via geolocation API

--- public/js/index.js
+++ public/js/index.js
@@ -2,9 +2,9 @@ var socket = io();
 
 socket.on('connect',function(){
     console.log('connected to server !');
-
-
 });
+
+
 
 socket.on('disconect',function(){
     console.log('disconnected from server !');
@@ -18,6 +18,14 @@ socket.on('newMessage',function(message){
     jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage',function(message){
+    var li = jQuery('<li></li>');
+    var a =jQuery('<a target="_blank">My current location</a>')
+    li.text(`${message.from} : `);
+    a.attr('href',message.url);
+    li.append(a);
+    jQuery('#messages').append(li);
+})
 
 jQuery('#message-form').on('submit',function(e){
     e.preventDefault();
@@ -28,4 +36,19 @@ jQuery('#message-form').on('submit',function(e){
     },function(){
         // CALL BACK MESSAGE.
     });
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click',function(){
+    if(!navigator.geolocation){
+        return alert('Geolocation not supported by your browser ');
+    }
+    navigator.geolocation.getCurrentPosition(function(position){
+        socket.emit('createLocationMessage',{
+            latitude : position.coords.latitude,
+            longitude : position.coords.longitude
+        });
+    },function(error){
+        alert('unable to fetch location give permission ',error);
+    })
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 var expect = require('expect');
-var {generateMessage} = require('./message');
+var {generateMessage,generateLocationMessage} = require('./message');
 
 describe('generateMessage',() => {
     it('should generate correct message object', () => {
@@ -10,3 +10,15 @@ describe('generateMessage',() => {
         expect(message).toInclude({from,text});
     });
 });
+
+describe('generateLocationMessage',() => {
+    it('should generate correct location object',()=>{
+        var from =' shyam';
+        var latitude = 15;
+        var longitude = 13;
+        var url = 'https://www.google.com/maps?q=15,13';
+        var message = generateLocationMessage(from,latitude,longitude);
+        expect(message.createAt).toBeA('number');
+        expect(message).toInclude({from,url});
+    });
+})
