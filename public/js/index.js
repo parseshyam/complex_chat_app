219ca486cdd2bda8cb62750f6f1cdbf9ad5ef995
Parse Shyam <parse.shyam@SHYAM-PC.local>
2019-01-25 18:22:27 +0530

    forma time stamp using moment js

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,5 @@
+
+
 var socket = io();
 
 socket.on('connect',function(){
@@ -12,16 +14,18 @@ socket.on('disconect',function(){
 
 
 socket.on('newMessage',function(message){
+    var formattedTime = moment(message.createAt).format('h:mm a');
     console.log('New message',message);
     var li = jQuery('<li></li>');
-    li.text(`${message.from} : ${message.text}`);
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
     jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage',function(message){
     var li = jQuery('<li></li>');
-    var a =jQuery('<a target="_blank">My current location</a>')
-    li.text(`${message.from} : `);
+    var a =jQuery('<a target="_blank">My current location</a>');
+    var formattedTime = moment(message.createAt).format('h:mm a');
+    li.text(`${message.from} ${formattedTime}: `);
     a.attr('href',message.url);
     li.append(a);
     jQuery('#messages').append(li);
--- server/utils/message.js
+++ server/utils/message.js
@@ -1,8 +1,9 @@
+var moment = require('moment');
 var generateMessage = (from,text) => {
     return {
         from : from,
         text : text,
-        createAt : new Date().getTime()
+        createAt : moment().valueOf()
     };
 };
 
@@ -10,7 +11,7 @@ var generateLocationMessage = (from,latitude,longitude) => {
     return{
         from,
         url : `https://www.google.com/maps?q=${latitude},${longitude}`,
-        createAt : new Date().getTime()
+        createAt : moment().valueOf()
         }
     }
 
